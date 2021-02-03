8f2dba4b1fb1ecc66f49e86f8b015565e0962e7d
Alex <alex@picknik.ai>
2021-02-03 09:22:04 -0700

    Switch compile distro to noetic

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -17,8 +17,8 @@ gem update --system
 gem --version
 gem install html-proofer
 # Install ROS's version of sphinx
-sudo apt-get -qq install ros-melodic-rosdoc-lite
-source /opt/ros/melodic/setup.bash
+sudo apt-get -qq install ros-noetic-rosdoc-lite
+source /opt/ros/noetic/setup.bash
 
 # Test build with non-ROS wrapped Sphinx command to allow warnings and errors to be caught
 sphinx-build -W -b html . native_build
