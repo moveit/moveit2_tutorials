c4d92f7cfa5c5f9fd896e55265ab1ee0810a02f0
Alex Goldman <alex.goldman@picknik.ai>
2021-03-10 10:15:29 -0700

    remove dependencies on rosdoc-lite to build

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -31,12 +31,11 @@ source install/setup.bash
 cd ../$REPOSITORY_NAME
 
 # Test build with non-ROS wrapped Sphinx command to allow warnings and errors to be caught
-sphinx-build -W -b html . native_build
-# Test build with ROS-version of Sphinx command so that it is generated same as ros.org
-rosdoc_lite -o build .
+sphinx-build -W -b html . build
+
 # Run HTML tests on generated build output to check for 404 errors, etc
-test "$TRAVIS_PULL_REQUEST" == false || URL_SWAP="--url-swap https\://github.com/ros-planning/moveit2_tutorials/blob/main/:file\://$PWD/build/html/"
-htmlproofer ./build --only-4xx --check-html --file-ignore ./build/html/genindex.html,./build/html/search.html --alt-ignore '/.*/' --url-ignore '#' $URL_SWAP
+test "$TRAVIS_PULL_REQUEST" == false || URL_SWAP="--url-swap https\://github.com/ros-planning/moveit2_tutorials/blob/main/:file\://$PWD/build/"
+htmlproofer ./build --only-4xx --check-html --file-ignore ./build/genindex.html,./build/search.html --alt-ignore '/.*/' --url-ignore '#' $URL_SWAP
 
 # Tell GitHub Pages (on deploy) to bypass Jekyll processing
-touch build/html/.nojekyll
+touch build/.nojekyll
