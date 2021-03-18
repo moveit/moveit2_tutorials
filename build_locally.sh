f21c59b0dc156efeb795e08e4c396e1b659bf44c
marqrazz <marq.razz@gmail.com>
2021-03-18 07:15:14 -0600

    added conditional install of sphinx to build_locally

--- build_locally.sh
+++ build_locally.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Install sphinx-build if it isn't there yet
+if ! type "sphinx-build" > /dev/null
+then
+  echo "Installing sphinx"
+  pip install sphinx==1.8.5
+fi
+
 # Setup Environment
 rm -rf build
 
