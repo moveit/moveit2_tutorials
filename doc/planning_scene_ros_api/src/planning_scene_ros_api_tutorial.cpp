fc0bff5c256a85e53b92beb015e0f67d35a51721
Vatan Aksoy Tezer <vatanaksoytezer@gmail.com>
2021-04-15 11:31:46 +0000

    Fix headers

--- doc/planning_scene_ros_api/src/planning_scene_ros_api_tutorial.cpp
+++ doc/planning_scene_ros_api/src/planning_scene_ros_api_tutorial.cpp
@@ -34,25 +34,25 @@
 
 /* Author: Sachin Chitta, Michael Lautman */
 
-#include <ros/ros.h>
-#include <geometry_msgs/Pose.h>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose.hpp>
 
 // MoveIt
-#include <moveit_msgs/PlanningScene.h>
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/GetStateValidity.h>
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/ApplyPlanningScene.h>
+#include <moveit_msgs/msg/planning_scene.hpp>
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+// #include <moveit_msgs/msg/GetStateValidity.h>
+#include <moveit_msgs/msg/display_robot_state.hpp>
+// #include <moveit_msgs/msg/ApplyPlanningScene.h>
 
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_state/conversions.h>
 
-#include <moveit_visual_tools/moveit_visual_tools.h>
+// #include <moveit_visual_tools/moveit_visual_tools.h>
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "planning_scene_ros_api_tutorial");
+  rclcpp::init(argc, argv, "planning_scene_ros_api_tutorial");
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
