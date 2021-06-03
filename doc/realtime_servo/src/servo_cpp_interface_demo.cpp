391810bdb5ab62015731189ffff69c22738f5398
AdamPettinger <adam.pettinger@utexas.edu>
2021-06-03 11:08:31 -0600

    Change C++ demo away from class-based example for readability

--- doc/realtime_servo/src/servo_cpp_interface_demo.cpp
+++ doc/realtime_servo/src/servo_cpp_interface_demo.cpp
@@ -52,145 +52,143 @@ static const rclcpp::Logger LOGGER = rclcpp::get_logger("moveit2_tutorials.servo
 
 // BEGIN_TUTORIAL
 
-class ServoCppDemo
+// Setup
+// ^^^^^
+// First we declare pointers to the node and publisher that will publish commands to Servo
+rclcpp::Node::SharedPtr node_;
+rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_cmd_pub_;
+rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_cmd_pub_;
+size_t count_ = 0;
+
+// BEGIN_SUB_TUTORIAL publishCommands
+// Here is the timer callback for publishing commands. The C++ interface sends commands through internal ROS topics,
+// just like if Servo was launched using ServoServer.
+void publishCommands()
 {
-public:
-  ServoCppDemo(rclcpp::Node::SharedPtr node) : node_(node), count_(0)
+  // First we will publish 100 joint jogging commands. The :code:`joint_names` field allows you to specify individual
+  // joints to move, at the velocity in the cooresponding :code:`velocities` field. It is important that the message
+  // contains a recent timestamp, or Servo will think the command is stale and will not move the robot.
+  if (count_ < 100)
   {
-    // Create the planning_scene_monitor
-    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(node_->get_clock());
-    planning_scene_monitor_ = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(
-        node_, "robot_description", tf_buffer_, "planning_scene_monitor");
-
-    // Get the planning_scene_monitor to publish scene diff's for RViz visualization
-    if (planning_scene_monitor_->getPlanningScene())
-    {
-      planning_scene_monitor_->startStateMonitor("/joint_states");
-      planning_scene_monitor_->setPlanningScenePublishingFrequency(25);
-      planning_scene_monitor_->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE,
-                                                            "/moveit_servo/publish_planning_scene");
-      planning_scene_monitor_->startSceneMonitor();
-    }
-    else
-    {
-      RCLCPP_ERROR(LOGGER, "Planning scene not configured");
-    }
-
-    // We need 2 different publishers for the different command types
-    joint_cmd_pub_ = node_->create_publisher<control_msgs::msg::JointJog>("servo_demo_node/delta_joint_cmds", 10);
-    twist_cmd_pub_ = node_->create_publisher<geometry_msgs::msg::TwistStamped>("servo_demo_node/delta_twist_cmds", 10);
+    auto msg = std::make_unique<control_msgs::msg::JointJog>();
+    msg->header.stamp = node_->now();
+    msg->joint_names.push_back("panda_joint1");
+    msg->velocities.push_back(0.3);
+    joint_cmd_pub_->publish(std::move(msg));
+    ++count_;
   }
 
-  bool start()
+  // After a while, we switch to publishing twist commands. The provided frame is the frame in which the twist is
+  // defined, not the robot frame that will follow the command. Again, we need a recent timestamp in the message
+  else
   {
-    // Get Servo Parameters
-    auto servo_parameters = moveit_servo::ServoParameters::makeServoParameters(node_, LOGGER);
-    if (!servo_parameters)
-    {
-      RCLCPP_FATAL(LOGGER, "Failed to load the servo parameters");
-      return false;
-    }
-
-    // Create Servo and start it
-    servo_ = std::make_unique<moveit_servo::Servo>(node_, servo_parameters, planning_scene_monitor_);
-    servo_->start();
-
-    timer_ = node_->create_wall_timer(50ms, std::bind(&ServoCppDemo::publishCommands, this));
-    return true;
+    auto msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
+    msg->header.stamp = node_->now();
+    msg->header.frame_id = "panda_link0";
+    msg->twist.linear.x = 0.3;
+    msg->twist.angular.z = 0.5;
+    twist_cmd_pub_->publish(std::move(msg));
   }
+}
+// END_SUB_TUTORIAL
 
-private:
-  rclcpp::Node::SharedPtr node_;
-
-  rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_cmd_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_cmd_pub_;
-
-  size_t count_;
-
-  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  std::shared_ptr<planning_scene_monitor::PlanningSceneMonitor> planning_scene_monitor_;
-  std::unique_ptr<moveit_servo::Servo> servo_;
-
-  rclcpp::TimerBase::SharedPtr timer_;
-
-  void publishCommands()
-  {
-    // Publish joint commands for a little bit
-    if (count_ < 100)
-    {
-      auto msg = std::make_unique<control_msgs::msg::JointJog>();
-      msg->header.stamp = node_->now();
-      msg->joint_names.push_back("panda_joint1");
-      msg->velocities.push_back(0.3);
-      joint_cmd_pub_->publish(std::move(msg));
-      ++count_;
-    }
-    // Then switch to twist commands
-    else
-    {
-      auto msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
-      msg->header.stamp = node_->now();
-      msg->header.frame_id = "panda_link0";
-      msg->twist.linear.x = 0.3;
-      msg->twist.angular.z = 0.5;
-      twist_cmd_pub_->publish(std::move(msg));
-    }
-  }
-};
-
+// Next we will set up the node, planning_scene_monitor, and collision object
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions node_options;
   node_options.use_intra_process_comms(true);
-  auto node = std::make_shared<rclcpp::Node>("servo_demo_node", node_options);
+  node_ = std::make_shared<rclcpp::Node>("servo_demo_node", node_options);
 
-  // Pause for RViz to come up..
+  // Pause for RViz to come up. This is necessary in an integrated demo with a single launch file
   rclcpp::sleep_for(std::chrono::seconds(4));
 
-  // We need to initialize the planning_scene_monitor here before publishing the collision object
-  ServoCppDemo demo(node);
+  // Create the planning_scene_monitor. We need to pass this to Servo's constructor, and we should set it up first
+  // before initializing any collision objects
+  auto tf_buffer = std::make_shared<tf2_ros::Buffer>(node_->get_clock());
+  auto planning_scene_monitor = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(
+      node_, "robot_description", tf_buffer, "planning_scene_monitor");
 
-  // Create collision object, in the way of servoing
+  // Here we make sure the planning_scene_monitor is updating in real time from the joint states topic
+  if (planning_scene_monitor->getPlanningScene())
+  {
+    planning_scene_monitor->startStateMonitor("/joint_states");
+    planning_scene_monitor->setPlanningScenePublishingFrequency(25);
+    planning_scene_monitor->startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE,
+                                                         "/moveit_servo/publish_planning_scene");
+    planning_scene_monitor->startSceneMonitor();
+  }
+  else
+  {
+    RCLCPP_ERROR(LOGGER, "Planning scene not configured");
+    return EXIT_FAILURE;
+  }
+
+  // These are the publishers that will send commands to MoveIt Servo. Two command types are supported: JointJog
+  // messages which will directly jog the robot in the joint space, and TwistStamped messages which will move the
+  // specified link with the commanded Cartesian velocity. In this demo, we jog the end effector link.
+  joint_cmd_pub_ = node_->create_publisher<control_msgs::msg::JointJog>("servo_demo_node/delta_joint_cmds", 10);
+  twist_cmd_pub_ = node_->create_publisher<geometry_msgs::msg::TwistStamped>("servo_demo_node/delta_twist_cmds", 10);
+
+  // Next we will create a collision object in the way of the arm. As the arm is servoed towards it, it will slow down
+  // and stop before colliding
   moveit_msgs::msg::CollisionObject collision_object;
   collision_object.header.frame_id = "panda_link0";
   collision_object.id = "box";
 
+  // Make a box and put it in the way
   shape_msgs::msg::SolidPrimitive box;
   box.type = box.BOX;
   box.dimensions = { 0.1, 0.4, 0.1 };
-
   geometry_msgs::msg::Pose box_pose;
   box_pose.position.x = 0.6;
   box_pose.position.y = 0.0;
   box_pose.position.z = 0.6;
 
+  // Add the box as a collision object
   collision_object.primitives.push_back(box);
   collision_object.primitive_poses.push_back(box_pose);
   collision_object.operation = collision_object.ADD;
 
+  // Create the message to publish the collision object
   moveit_msgs::msg::PlanningSceneWorld psw;
   psw.collision_objects.push_back(collision_object);
-
   moveit_msgs::msg::PlanningScene ps;
   ps.is_diff = true;
   ps.world = psw;
 
   // Publish the collision object to the planning scene
-  auto scene_pub = node->create_publisher<moveit_msgs::msg::PlanningScene>("planning_scene", 10);
+  auto scene_pub = node_->create_publisher<moveit_msgs::msg::PlanningScene>("planning_scene", 10);
   scene_pub->publish(ps);
 
-  // Start the Servo object, and start publishing commands to it
-  if (!demo.start())
+  // Initializing Servo
+  // ^^^^^^^^^^^^^^^^^^
+  // Servo requires a number of parameters to dictate its behavoir. These can be read automatically by using the
+  // :code:`makeServoParameters` helper function
+  auto servo_parameters = moveit_servo::ServoParameters::makeServoParameters(node_, LOGGER);
+  if (!servo_parameters)
   {
-    RCLCPP_FATAL(LOGGER, "Failed to start");
-    rclcpp::shutdown();
-    return 1;
+    RCLCPP_FATAL(LOGGER, "Failed to load the servo parameters");
+    return EXIT_FAILURE;
   }
 
-  // Spin
+  // Initialize the Servo C++ interface by passing a pointer to the node, the parameters, and the PSM
+  auto servo = std::make_unique<moveit_servo::Servo>(node_, servo_parameters, planning_scene_monitor);
+
+  // You can start Servo directly using the C++ interface. If launched using the alternative ServoServer, a ROS service
+  // is used to start Servo. Before it is started, MoveIt Servo will not accept any commands or move the robot
+  servo->start();
+
+  // Sending Commands
+  // ^^^^^^^^^^^^^^^^
+  // For this demo, we will use a simple ROS timer to send joint and twist commands to the robot
+  rclcpp::TimerBase::SharedPtr timer = node_->create_wall_timer(50ms, publishCommands);
+
+  // CALL_SUB_TUTORIAL publishCommands
+
+  // We use a multithreaded executor here because Servo has concurrent processes for moving the robot and avoiding collisions
   auto executor = std::make_unique<rclcpp::executors::MultiThreadedExecutor>();
-  executor->add_node(node);
+  executor->add_node(node_);
   executor->spin();
 
   // END_TUTORIAL
