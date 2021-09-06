9db1802dfb1a4a1c638014ce90420be35727ec61
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2021-09-06 11:40:42 +0100

    Migrated robot model and robot state tutorial to moveit2

--- doc/robot_model_and_robot_state/src/robot_model_and_robot_state_tutorial.cpp
+++ doc/robot_model_and_robot_state/src/robot_model_and_robot_state_tutorial.cpp
@@ -34,129 +34,142 @@
 
 /* Author: Sachin Chitta, Michael Lautman*/
 
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
 // MoveIt
-#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_state/robot_state.h>
+#include <moveit/planning_scene/planning_scene.h>
 
-int main(int argc, char** argv)
+
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("robot_model_and_robot_state_node");
+
+int main(int argc, char** argv) 
 {
-  ros::init(argc, argv, "robot_model_and_robot_state_tutorial");
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions _node_option;
+    _node_option.automatically_declare_parameters_from_overrides(true);
 
-  // BEGIN_TUTORIAL
-  // Start
-  // ^^^^^
-  // Setting up to start using the RobotModel class is very easy. In
-  // general, you will find that most higher-level components will
-  // return a shared pointer to the RobotModel. You should always use
-  // that when possible. In this example, we will start with such a
-  // shared pointer and discuss only the basic API. You can have a
-  // look at the actual code API for these classes to get more
-  // information about how to use more features provided by these
-  // classes.
-  //
-  // We will start by instantiating a
-  // `RobotModelLoader`_
-  // object, which will look up
-  // the robot description on the ROS parameter server and construct a
-  // :moveit_core:`RobotModel` for us to use.
-  //
-  // .. _RobotModelLoader:
-  //     http://docs.ros.org/noetic/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
-  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-  const moveit::core::RobotModelPtr& kinematic_model = robot_model_loader.getModel();
-  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
+    auto robot_model_and_robot_state_node = rclcpp::Node::make_shared("robot_model_and_robot_state_node", _node_option);
 
-  // Using the :moveit_core:`RobotModel`, we can construct a
-  // :moveit_core:`RobotState` that maintains the configuration
-  // of the robot. We will set all joints in the state to their
-  // default values. We can then get a
-  // :moveit_core:`JointModelGroup`, which represents the robot
-  // model for a particular group, e.g. the "panda_arm" of the Panda
-  // robot.
-  moveit::core::RobotStatePtr kinematic_state(new moveit::core::RobotState(kinematic_model));
-  kinematic_state->setToDefaultValues();
-  const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("panda_arm");
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(robot_model_and_robot_state_node);
+    std::thread([&executor]() { executor.spin(); }).detach();
 
-  const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
+    // BEGIN_TUTORIAL
+    // Start
+    // ^^^^^
+    // Setting up to start using the RobotModel class is very easy. In
+    // general, you will find that most higher-level components will
+    // return a shared pointer to the RobotModel. You should always use
+    // that when possible. In this example, we will start with such a
+    // shared pointer and discuss only the basic API. You can have a
+    // look at the actual code API for these classes to get more
+    // information about how to use more features provided by these
+    // classes.
+    //
+    // We will start by instantiating a
+    // `RobotModelLoader`_
+    // object, which will look up
+    // the robot description on the ROS parameter server and construct a
+    // :moveit_core:`RobotModel` for us to use.
+    //
+    // .. _RobotModelLoader:
+    //     http://docs.ros.org/noetic/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
+    robot_model_loader::RobotModelLoader robot_model_loader(robot_model_and_robot_state_node, "robot_description");
+    const moveit::core::RobotModelPtr& kinematic_model = robot_model_loader.getModel();
+    RCLCPP_INFO(LOGGER, "Model frame: %s", kinematic_model->getModelFrame().c_str());
 
-  // Get Joint Values
-  // ^^^^^^^^^^^^^^^^
-  // We can retreive the current set of joint values stored in the state for the Panda arm.
-  std::vector<double> joint_values;
-  kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-  for (std::size_t i = 0; i < joint_names.size(); ++i)
-  {
-    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
-  }
 
-  // Joint Limits
-  // ^^^^^^^^^^^^
-  // setJointGroupPositions() does not enforce joint limits by itself, but a call to enforceBounds() will do it.
-  /* Set one joint in the Panda arm outside its joint limit */
-  joint_values[0] = 5.57;
-  kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
+    // Using the :moveit_core:`RobotModel`, we can construct a
+    // :moveit_core:`RobotState` that maintains the configuration
+    // of the robot. We will set all joints in the state to their
+    // default values. We can then get a
+    // :moveit_core:`JointModelGroup`, which represents the robot
+    // model for a particular group, e.g. the "panda_arm" of the Panda
+    // robot.
+    moveit::core::RobotStatePtr kinematic_state(new moveit::core::RobotState(kinematic_model));
+    kinematic_state->setToDefaultValues();
+    const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("panda_arm");
 
-  /* Check whether any joint is outside its joint limits */
-  ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
+    const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
 
-  /* Enforce the joint limits for this state and check again*/
-  kinematic_state->enforceBounds();
-  ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
 
-  // Forward Kinematics
-  // ^^^^^^^^^^^^^^^^^^
-  // Now, we can compute forward kinematics for a set of random joint
-  // values. Note that we would like to find the pose of the
-  // "panda_link8" which is the most distal link in the
-  // "panda_arm" group of the robot.
-  kinematic_state->setToRandomPositions(joint_model_group);
-  const Eigen::Isometry3d& end_effector_state = kinematic_state->getGlobalLinkTransform("panda_link8");
-
-  /* Print end-effector pose. Remember that this is in the model frame */
-  ROS_INFO_STREAM("Translation: \n" << end_effector_state.translation() << "\n");
-  ROS_INFO_STREAM("Rotation: \n" << end_effector_state.rotation() << "\n");
-
-  // Inverse Kinematics
-  // ^^^^^^^^^^^^^^^^^^
-  // We can now solve inverse kinematics (IK) for the Panda robot.
-  // To solve IK, we will need the following:
-  //
-  //  * The desired pose of the end-effector (by default, this is the last link in the "panda_arm" chain):
-  //    end_effector_state that we computed in the step above.
-  //  * The timeout: 0.1 s
-  double timeout = 0.1;
-  bool found_ik = kinematic_state->setFromIK(joint_model_group, end_effector_state, timeout);
-
-  // Now, we can print out the IK solution (if found):
-  if (found_ik)
-  {
+    // Get Joint Values
+    // ^^^^^^^^^^^^^^^^
+    // We can retreive the current set of joint values stored in the state for the Panda arm.
+    std::vector<double> joint_values;
     kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
     for (std::size_t i = 0; i < joint_names.size(); ++i)
     {
-      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+        RCLCPP_INFO(LOGGER, "Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
     }
-  }
-  else
-  {
-    ROS_INFO("Did not find IK solution");
-  }
 
-  // Get the Jacobian
-  // ^^^^^^^^^^^^^^^^
-  // We can also get the Jacobian from the :moveit_core:`RobotState`.
-  Eigen::Vector3d reference_point_position(0.0, 0.0, 0.0);
-  Eigen::MatrixXd jacobian;
-  kinematic_state->getJacobian(joint_model_group,
-                               kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
-                               reference_point_position, jacobian);
-  ROS_INFO_STREAM("Jacobian: \n" << jacobian << "\n");
-  // END_TUTORIAL
+    // Joint Limits
+    // ^^^^^^^^^^^^
+    // setJointGroupPositions() does not enforce joint limits by itself, but a call to enforceBounds() will do it.
+    /* Set one joint in the Panda arm outside its joint limit */
+    joint_values[0] = 5.57;
+    kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
 
-  ros::shutdown();
-  return 0;
+    /* Check whether any joint is outside its joint limits */
+    RCLCPP_INFO(LOGGER, "Current state is %s", (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
+    
+    /* Enforce the joint limits for this state and check again*/
+    kinematic_state->enforceBounds();
+    RCLCPP_INFO(LOGGER, "Current state is %s", (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
+    
+    /// Forward Kinematics
+    // ^^^^^^^^^^^^^^^^^^
+    // Now, we can compute forward kinematics for a set of random joint
+    // values. Note that we would like to find the pose of the
+    // "panda_link8" which is the most distal link in the
+    // "panda_arm" group of the robot.
+    kinematic_state->setToRandomPositions(joint_model_group);
+    const Eigen::Isometry3d& end_effector_state = kinematic_state->getGlobalLinkTransform("panda_link8");
+
+    /* Print end-effector pose. Remember that this is in the model frame */
+    RCLCPP_INFO_STREAM(LOGGER, "Translation: \n" << end_effector_state.translation() << "\n");
+    RCLCPP_INFO_STREAM(LOGGER, "Rotation: \n" << end_effector_state.rotation() << "\n");
+
+    // Inverse Kinematics
+    // ^^^^^^^^^^^^^^^^^^
+    // We can now solve inverse kinematics (IK) for the Panda robot.
+    // To solve IK, we will need the following:
+    //
+    //  * The desired pose of the end-effector (by default, this is the last link in the "panda_arm" chain):
+    //    end_effector_state that we computed in the step above.
+    //  * The timeout: 0.1 s
+    double timeout = 0.1;
+    bool found_ik = kinematic_state->setFromIK(joint_model_group, end_effector_state, timeout);
+
+    // Now, we can print out the IK solution (if found):
+    if (found_ik)
+    {
+        kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+        for (std::size_t i = 0; i < joint_names.size(); ++i) {
+            RCLCPP_INFO(LOGGER, "Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+        }
+    }
+    else 
+    {
+        RCLCPP_INFO(LOGGER, "Did not find IK Solution");
+    }
+
+    // Get the Jacobian
+    // ^^^^^^^^^^^^^^^^
+    // We can also get the Jacobian from the :moveit_core:`RobotState`.
+    Eigen::Vector3d reference_point_position(0.0, 0.0, 0.0);
+    Eigen::MatrixXd jacobian;
+    kinematic_state->getJacobian(joint_model_group,
+                                 kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
+                                 reference_point_position, jacobian);
+
+    RCLCPP_INFO_STREAM(LOGGER, "Jacobian: \n" << jacobian << "\n");
+    // END_TUTORIAL
+
+    rclcpp::shutdown();
+    return 0;
 }
