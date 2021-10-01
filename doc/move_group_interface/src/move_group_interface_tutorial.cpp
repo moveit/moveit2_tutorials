b19010c672b37f7f936ed5e9826d48b5617df705
Vatan Aksoy Tezer <vatan@picknik.ai>
2021-10-01 22:05:50 +0300

    use mvt instead of rvt

--- doc/move_group_interface/src/move_group_interface_tutorial.cpp
+++ doc/move_group_interface/src/move_group_interface_tutorial.cpp
@@ -43,9 +43,8 @@
 #include <moveit_msgs/msg/attached_collision_object.hpp>
 #include <moveit_msgs/msg/collision_object.hpp>
 
-/* #include <moveit_visual_tools/moveit_visual_tools.h>  This has not been ported to ros2 yet */
-#include <rviz_visual_tools/rviz_visual_tools.hpp>
-/* this is a standin for moveit_visual_tools visual_tools.prompt */
+#include <moveit_visual_tools/moveit_visual_tools.h> 
+
 #include <moveit/macros/console_colors.h>
 void prompt(const std::string& message)
 {
@@ -97,14 +96,10 @@ int main(int argc, char** argv)
 
   // Visualization
   // ^^^^^^^^^^^^^
-  //
-  // MoveitVisualTools has not been ported to ROS2 yet so we make use of RvizVisualTools for visualization.
-  // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
-  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script.
-
   namespace rvt = rviz_visual_tools;
-  rviz_visual_tools::RvizVisualTools visual_tools("panda_link0", "move_group_tutorial", move_group_node);
-  /* moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0"); */
+  moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "panda_link0", "move_group_tutorial", move_group.getRobotModel());
+  // moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "panda_link0", "move_group_tutorial");
+
   visual_tools.deleteAllMarkers();
 
   /* Remote control is an introspection tool that allows users to step through a high level script */
@@ -165,7 +160,7 @@ int main(int argc, char** argv)
   RCLCPP_INFO(LOGGER, "Visualizing plan 1 as trajectory line");
   visual_tools.publishAxisLabeled(target_pose1, "pose1");
   visual_tools.publishText(text_pose, "Pose_Goal", rvt::WHITE, rvt::XLARGE);
-  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group); */
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
@@ -214,7 +209,7 @@ int main(int argc, char** argv)
   //  Visualize the plan in RViz
   visual_tools.deleteAllMarkers();
   visual_tools.publishText(text_pose, "Joint_Space_Goal", rvt::WHITE, rvt::XLARGE);
-  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group); */
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
@@ -283,7 +278,7 @@ int main(int argc, char** argv)
   visual_tools.publishAxisLabeled(start_pose2, "start");
   visual_tools.publishAxisLabeled(target_pose1, "goal");
   visual_tools.publishText(text_pose, "Constrained_Goal", rvt::WHITE, rvt::XLARGE);
-  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group); */
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
@@ -359,7 +354,7 @@ int main(int argc, char** argv)
   visual_tools.deleteAllMarkers();
   visual_tools.publishText(text_pose, "Clear_Goal", rvt::WHITE, rvt::XLARGE);
   visual_tools.publishAxisLabeled(another_pose, "goal");
-  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group); */
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
@@ -405,14 +400,13 @@ int main(int argc, char** argv)
   // Show text in RViz of status and wait for MoveGroup to receive and process the collision object message
   visual_tools.publishText(text_pose, "Add_object", rvt::WHITE, rvt::XLARGE);
   visual_tools.trigger();
-  // prompt("Press 'Enter' to continue once the collision object appears in RViz");
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to once the collision object appears in RViz");
 
   // Now when we plan a trajectory it will avoid the obstacle
   success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   RCLCPP_INFO(LOGGER, "Visualizing plan 6 (pose goal move around cuboid) %s", success ? "" : "FAILED");
   visual_tools.publishText(text_pose, "Obstacle_Goal", rvt::WHITE, rvt::XLARGE);
-  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group); */
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window once the plan is complete");
 
@@ -467,7 +461,7 @@ int main(int argc, char** argv)
   move_group.setStartStateToCurrentState();
   success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   RCLCPP_INFO(LOGGER, "Visualizing plan 7 (move around cuboid with cylinder) %s", success ? "" : "FAILED");
-  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group); */
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window once the plan is complete");
 
