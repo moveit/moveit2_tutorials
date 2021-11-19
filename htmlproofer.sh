58aa9088daef07715c719059588de7ce0076ffac
Vatan Aksoy Tezer <vatan@picknik.ai>
2021-11-19 19:56:07 +0300

    Fix htmlproofer url swap

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -20,7 +20,7 @@ rm -rf build
 sphinx-build -W -b html . build
 
 # Run HTML tests on generated build output to check for 404 errors, etc
-[[ -v GITHUB_HEAD_REF ]] || URL_SWAP="--url-swap https\://github.com/ros-planning/moveit2_tutorials/blob/main/:file\://$PWD/build/"
+[[ -v GITHUB_HEAD_REF ]] || URL_SWAP="--url-swap https\://github.com/ros-planning/moveit2_tutorials/blob/main/:file\://$PWD/build/ --url-swap https\://moveit.picknik.ai/rolling:file\://$PWD/build/"
 htmlproofer ./build --only-4xx --check-html --file-ignore ./build/genindex.html,./build/search.html --alt-ignore '/.*/' --url-ignore '#' $URL_SWAP
 
 # Tell GitHub Pages (on deploy) to bypass Jekyll processing
