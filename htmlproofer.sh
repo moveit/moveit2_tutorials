e52e7b36b543f7b672c6c00743959f9bd7989006
Vatan Aksoy Tezer <vatan@picknik.ai>
2021-11-19 21:25:32 +0300

    Remove build folder before htmlproofer and make sure html urls are swapped (#218)

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -13,12 +13,14 @@ PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
 # Install python dependencies
 pip3 install --user --upgrade -r requirements.txt
 
+# Clear out any previous builds
+rm -rf build
+
 # Test build with non-ROS wrapped Sphinx command to allow warnings and errors to be caught
 sphinx-build -W -b html . build
 
 # Run HTML tests on generated build output to check for 404 errors, etc
-[[ -v GITHUB_HEAD_REF ]] || URL_SWAP="--url-swap https\://github.com/ros-planning/moveit2_tutorials/blob/foxy/:file\://$PWD/build/"
-htmlproofer ./build --only-4xx --check-html --file-ignore ./build/genindex.html,./build/search.html --alt-ignore '/.*/' --url-ignore '#' $URL_SWAP
+htmlproofer ./build --only-4xx --check-html --file-ignore ./build/genindex.html,./build/search.html --alt-ignore '/.*/' --url-ignore '#' --url-swap 'https\://github.com/ros-planning/moveit2_tutorials/blob/foxy/:file\://$PWD/build/' --url-swap 'https\://moveit.picknik.ai/foxy:file\://$PWD/build/'
 
 # Tell GitHub Pages (on deploy) to bypass Jekyll processing
 touch build/.nojekyll
