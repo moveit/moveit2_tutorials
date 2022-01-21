8238b2c51c7d1565a34e71866294df7c8ee421e6
Jafar Abdi <cafer.abdi@gmail.com>
2022-01-21 12:21:22 -0700

    Use moveit configs utils packages (#243)
    Co-authored-by: Tyler Weaver <tylerjw@gmail.com>

--- doc/examples/move_group_interface/launch/move_group_interface_tutorial.launch.py
+++ doc/examples/move_group_interface/launch/move_group_interface_tutorial.launch.py
@@ -1,49 +1,10 @@
-import os
-import yaml
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return file.read()
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def generate_launch_description():
-    # planning_context
-    robot_description_config = load_file(
-        "moveit_resources_panda_description", "urdf/panda.urdf"
-    )
-    robot_description = {"robot_description": robot_description_config}
-
-    robot_description_semantic_config = load_file(
-        "moveit_resources_panda_moveit_config", "config/panda.srdf"
-    )
-    robot_description_semantic = {
-        "robot_description_semantic": robot_description_semantic_config
-    }
-
-    kinematics_yaml = load_yaml(
-        "moveit_resources_panda_moveit_config", "config/kinematics.yaml"
-    )
+    moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_moveit_configs()
 
     # MoveGroupInterface demo executable
     move_group_demo = Node(
@@ -51,7 +12,11 @@ def generate_launch_description():
         package="moveit2_tutorials",
         executable="move_group_interface_tutorial",
         output="screen",
-        parameters=[robot_description, robot_description_semantic, kinematics_yaml],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
     )
 
     return LaunchDescription([move_group_demo])
