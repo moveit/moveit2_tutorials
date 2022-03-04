f674a2bd42c49387704a8200410e3e864a3df23a
AndyZe <zelenak@picknik.ai>
2022-03-03 21:18:07 -0600

    Update yaml names

--- doc/examples/realtime_servo/launch/servo_cpp_interface_demo.launch.py
+++ doc/examples/realtime_servo/launch/servo_cpp_interface_demo.launch.py
@@ -79,7 +79,7 @@ def generate_launch_description():
             os.path.join(
                 get_package_share_directory("moveit_resources_panda_moveit_config"),
                 "config",
-                "panda_ros_controllers.yaml",
+                "panda_ros2_controllers.yaml",
             ),
         ],
         output={
--- doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
+++ doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
@@ -76,7 +76,7 @@ def generate_launch_description():
 
     # Trajectory Execution Functionality
     moveit_simple_controllers_yaml = load_yaml(
-        "moveit_resources_panda_moveit_config", "config/panda_controllers.yaml"
+        "moveit_resources_panda_moveit_config", "config/panda_moveit_controllers.yaml"
     )
     moveit_controllers = {
         "moveit_simple_controller_manager": moveit_simple_controllers_yaml,
@@ -169,7 +169,7 @@ def generate_launch_description():
     ros2_controllers_path = os.path.join(
         get_package_share_directory("moveit_resources_panda_moveit_config"),
         "config",
-        "panda_ros_controllers.yaml",
+        "panda_ros2_controllers.yaml",
     )
     ros2_control_node = Node(
         package="controller_manager",
