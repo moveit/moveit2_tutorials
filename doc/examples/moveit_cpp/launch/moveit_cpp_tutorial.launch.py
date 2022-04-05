1ba524d75e551be62cc15d880efcda29a1df0737
David V. Lu!! <davidvlu@gmail.com>
2022-04-05 16:22:37 -0400

    Update panda controller filenames (#343)

--- doc/examples/moveit_cpp/launch/moveit_cpp_tutorial.launch.py
+++ doc/examples/moveit_cpp/launch/moveit_cpp_tutorial.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
     moveit_config = (
         MoveItConfigsBuilder("moveit_resources_panda")
         .robot_description(file_path="config/panda.urdf.xacro")
-        .trajectory_execution(file_path="config/panda_gripper_controllers.yaml")
+        .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
         .moveit_cpp(
             file_path=get_package_share_directory("moveit2_tutorials")
             + "/config/moveit_cpp.yaml"
@@ -65,7 +65,7 @@ def generate_launch_description():
     ros2_controllers_path = os.path.join(
         get_package_share_directory("moveit_resources_panda_moveit_config"),
         "config",
-        "panda_ros_controllers.yaml",
+        "ros2_controllers.yaml",
     )
     ros2_control_node = Node(
         package="controller_manager",
