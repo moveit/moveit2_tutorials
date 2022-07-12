9d468f227ce0e3666de553f9ef976c86d8859235
MikeWrock <michael.wrock@picknik.ai>
2022-07-12 10:53:26 -0700

    fixed clang complaints

--- doc/examples/collision_environments/src/collision_scene_example.cpp
+++ doc/examples/collision_environments/src/collision_scene_example.cpp
@@ -11,13 +11,12 @@
 // TF2
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
-moveit_msgs::msg::CollisionObject make_box(std::string name, std::string frame_id,
-                                               float length, float width, float height, 
-                                               float x, float y, float z, 
-                                               geometry_msgs::msg::Quaternion orientation){
-
+moveit_msgs::msg::CollisionObject make_box(std::string name, std::string frame_id, float length, float width,
+                                           float height, float x, float y, float z,
+                                           geometry_msgs::msg::Quaternion orientation)
+{
   moveit_msgs::msg::CollisionObject box;
-  
+
   // Add the first box relative to the base link's frame.
   box.id = name.c_str();
   box.header.frame_id = frame_id.c_str();
@@ -40,7 +39,6 @@ moveit_msgs::msg::CollisionObject make_box(std::string name, std::string frame_i
   box.operation = box.ADD;
 
   return box;
-
 }
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("collision_scene_example");
 
@@ -51,7 +49,7 @@ int main(int argc, char** argv)
   node_options.automatically_declare_parameters_from_overrides(true);
   auto collision_scene_example_node = rclcpp::Node::make_shared("planning_scene_tutorial", node_options);
 
-  //Check for robot_description_semantic param
+  // Check for robot_description_semantic param
 
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
   // Create vector to hold 4 collision objects.
@@ -69,7 +67,6 @@ int main(int argc, char** argv)
   collision_objects[2] = make_box("box3", "panda_link0", 0.28, 0.28, 0.22, 0.50, -.55, 0.14, zero_orientation_msg);
   collision_objects[3] = make_box("box4", "panda_link0", 0.25, 0.25, 1.10, -0.4, 0.40, 0.50, zero_orientation_msg);
 
-  
   planning_scene_interface_.applyCollisionObjects(collision_objects);
 
   rclcpp::shutdown();
