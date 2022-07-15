b108b0e8ad22f2190dfaf8f393c2d196b47b2945
Sebastian Jahr <sebastian.jahr@picknik.ai>
2022-07-15 18:11:27 +0200

    Add tutorial description and experiments

--- doc/how_to_guides/create_better_paths/path_quality.md
+++ doc/how_to_guides/create_better_paths/path_quality.md
@@ -1,2 +1,64 @@
-# How to improve MoveIt2 path quality?
-## What is path quality?
+How to customize path planning with MoveItCpp?
+==============================================
+
+This guide should help you to get the paths with MoveItCpp you want!
+
+Learning Objectives
+-------------------
+- What is path (planning) quality?
+- Composition of the MoveItCpp planning pipeline
+- Practical tipps to customize the path quality
+
+What is path (planning) quality?
+--------------------------------
+Internals of the MoveItCpp planning pipeline
+--------------------------------------------
+Use a distance aware IK solver
+------------------------------
+
+.. code-block:: yaml
+
+    kinematics_solver: bio_ik/BioIKKinematicsPlugin
+    kinematics_solver_search_resolution: 0.005
+    kinematics_solver_timeout: 0.005
+    kinematics_solver_attempts: 1.0
+    # optional bio_ik configuration parameters
+    center_joints_weight: 1.0
+    minimal_displacement_weight: 1.0
+    avoid_joint_limits_weight: 1.0
+    mode: "gd_c"
+
+Plan with multiple planning pipelines in parallel and select the most suitable solution
+---------------------------------------------------------------------------------------
+
+.. code-block:: yaml
+
+    one:
+    plan_request_params:
+      planning_attempts: 1
+      planning_pipeline: ompl
+      planner_id: "RRTConnectkConfigDefault"
+      max_velocity_scaling_factor: 1.0
+      max_acceleration_scaling_factor: 1.0
+
+    two:
+      plan_request_params:
+        planning_attempts: 1
+        planning_pipeline: ompl
+        planner_id: "RRTConnectkConfigDefault"
+        max_velocity_scaling_factor: 1.0
+        max_acceleration_scaling_factor: 1.0
+
+    three:
+      plan_request_params:
+        planning_attempts: 1
+        planning_pipeline: ompl
+        planner_id: "RRTConnectkConfigDefault"
+        max_velocity_scaling_factor: 1.0
+        max_acceleration_scaling_factor: 1.0
+
+Run the demo:
+
+.. code-block:: bash
+
+    ros2 launch moveit2_tutorials better_paths_demo.launch.py
--- doc/how_to_guides/create_better_paths/src/better_paths_main.cpp
+++ doc/how_to_guides/create_better_paths/src/better_paths_main.cpp
@@ -68,76 +68,76 @@ int main(int argc, char** argv)
 
   planning_components->setStartStateToCurrentState();
 
+  // Benchmark function
+  // Compute and print average path length, path similarity
+  auto analyzeResult = [&](std::vector<moveit_cpp::PlanningComponent::PlanSolution> solutions) {
+    // Adapted from https://github.com/ros-planning/moveit2/blob/main/moveit_ros/benchmarks/src/BenchmarkExecutor.cpp#L872
+    // Analyzing the trajectories geometrically
+    auto average_traj_len = 0.0;
+    auto average_path_simularity = 0.0;
 
-// Benchmark function
-// Compute and print average path length, path similarity
-auto analyzeResult = [&](std::vector<moveit_cpp::PlanningComponent::PlanSolution> solutions){
-
-  // Adapted from https://github.com/ros-planning/moveit2/blob/main/moveit_ros/benchmarks/src/BenchmarkExecutor.cpp#L872
-  // Analyzing the trajectories geometrically
-  auto average_traj_len = 0.0;
-  auto average_path_simularity = 0.0;
-
-  // Calculate average path length
-  for(auto solution : solutions){
-    // Only process successful solutions
-    if(solution.error_code == moveit::core::MoveItErrorCode::SUCCESS){
-      // Compute path length
-      for (std::size_t index = 1; index < solution.trajectory->getWayPointCount(); ++index)
-        average_traj_len += solution.trajectory->getWayPoint(index - 1).distance(solution.trajectory->getWayPoint(index));
-    }
-    }
-
-  if(solutions.size() > 0){
-    average_traj_len /= solutions.size();
-  }
-
-// Path similarity
-
-  RCLCPP_INFO_STREAM(LOGGER, "Average path length (sum of L1 norms): '" << average_traj_len << " rad'");
-  RCLCPP_INFO_STREAM(LOGGER, "Average path similarity: '" << average_path_simularity << "'");
-};
-
-// Some helper functions
-auto printAndPlan = [&](auto num_iterations, auto execute = false){
-  std::vector<moveit_cpp::PlanningComponent::PlanSolution> solutions;
-  solutions.reserve(num_iterations);
-  for(auto i = 0; i < num_iterations; i++)
-  {
-    planning_components->setStartStateToCurrentState();
-
-    moveit_cpp::PlanningComponent::MultiPipelinePlanRequestParameters multi_pipeline_plan_request;
-    multi_pipeline_plan_request.load(node, {"one", "two", "three"});
-
-
-    auto plan_solution = planning_components->plan(multi_pipeline_plan_request);
-
-    // Check if PlanningComponents succeeded in finding the plan
-    if (plan_solution)
+    // Calculate average path length
+    for (auto solution : solutions)
     {
-      // Visualize the trajectory in Rviz
-      visual_tools.publishTrajectoryLine(plan_solution.trajectory, joint_model_group_ptr);
-      visual_tools.trigger();
-      solutions.push_back(plan_solution);
+      // Only process successful solutions
+      if (solution.error_code == moveit::core::MoveItErrorCode::SUCCESS)
+      {
+        // Compute path length
+        for (std::size_t index = 1; index < solution.trajectory->getWayPointCount(); ++index)
+          average_traj_len +=
+              solution.trajectory->getWayPoint(index - 1).distance(solution.trajectory->getWayPoint(index));
+      }
     }
-    // Start the next plan
-    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
-    visual_tools.deleteAllMarkers();
-    visual_tools.trigger();
-      if(execute)
+
+    if (solutions.size() > 0)
+    {
+      average_traj_len /= solutions.size();
+    }
+
+    // Path similarity
+
+    RCLCPP_INFO_STREAM(LOGGER, "Average path length (sum of L1 norms): '" << average_traj_len << " rad'");
+    RCLCPP_INFO_STREAM(LOGGER, "Average path similarity: '" << average_path_simularity << "'");
+  };
+
+  // Some helper functions
+  auto printAndPlan = [&](auto num_iterations, auto execute = false) {
+    std::vector<moveit_cpp::PlanningComponent::PlanSolution> solutions;
+    solutions.reserve(num_iterations);
+    for (auto i = 0; i < num_iterations; i++)
+    {
+      planning_components->setStartStateToCurrentState();
+
+      moveit_cpp::PlanningComponent::MultiPipelinePlanRequestParameters multi_pipeline_plan_request;
+      multi_pipeline_plan_request.load(node, { "one", "two", "three" });
+
+      auto plan_solution = planning_components->plan(multi_pipeline_plan_request);
+
+      // Check if PlanningComponents succeeded in finding the plan
+      if (plan_solution)
+      {
+        // Visualize the trajectory in Rviz
+        visual_tools.publishTrajectoryLine(plan_solution.trajectory, joint_model_group_ptr);
+        visual_tools.trigger();
+        solutions.push_back(plan_solution);
+      }
+      // Start the next plan
+      visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+      visual_tools.deleteAllMarkers();
+      visual_tools.trigger();
+      if (execute)
       {
         planning_components->execute();
       }
-
     }
 
     // Print result
     analyzeResult(solutions);
-    };
+  };
 
-auto setJointGoal = [&](double const panda_joint1, double const panda_joint2, 
-  double const panda_joint3, double const panda_joint4, double const panda_joint5,
-  double const panda_joint6, double const panda_joint7){
+  auto setJointGoal = [&](double const panda_joint1, double const panda_joint2, double const panda_joint3,
+                          double const panda_joint4, double const panda_joint5, double const panda_joint6,
+                          double const panda_joint7) {
     auto robot_goal_state = planning_components->getStartState();
     robot_goal_state->setJointPositions("panda_joint1", &panda_joint1);
     robot_goal_state->setJointPositions("panda_joint2", &panda_joint2);
@@ -148,152 +148,149 @@ auto setJointGoal = [&](double const panda_joint1, double const panda_joint2,
     robot_goal_state->setJointPositions("panda_joint3", &panda_joint3);
 
     planning_components->setGoal(*robot_goal_state);
-};
+  };
 
-auto setCartGoal = [&](double const x, double const y, double const z, double const qx, double const qy,
-  double const qz, double const qw){
-  geometry_msgs::msg::PoseStamped target_pose;
-  target_pose.header.frame_id = "panda_link0";
-  target_pose.pose.orientation.x = qx;
-  target_pose.pose.orientation.y = qy;
-  target_pose.pose.orientation.z = qz;
-  target_pose.pose.orientation.w = qw;
-  target_pose.pose.position.x = x;
-  target_pose.pose.position.y = y;
-  target_pose.pose.position.z = z;
-  planning_components->setGoal(target_pose, "panda_link8");
-};
+  auto setCartGoal = [&](double const x, double const y, double const z, double const qx, double const qy,
+                         double const qz, double const qw) {
+    geometry_msgs::msg::PoseStamped target_pose;
+    target_pose.header.frame_id = "panda_link0";
+    target_pose.pose.orientation.x = qx;
+    target_pose.pose.orientation.y = qy;
+    target_pose.pose.orientation.z = qz;
+    target_pose.pose.orientation.w = qw;
+    target_pose.pose.position.x = x;
+    target_pose.pose.position.y = y;
+    target_pose.pose.position.z = z;
+    planning_components->setGoal(target_pose, "panda_link8");
+  };
 
-auto add_collision_objects = [&](){
-  moveit_msgs::msg::CollisionObject collision_object_1;
-  moveit_msgs::msg::CollisionObject collision_object_2;
-  moveit_msgs::msg::CollisionObject collision_object_3;
+  auto add_collision_objects = [&]() {
+    moveit_msgs::msg::CollisionObject collision_object_1;
+    moveit_msgs::msg::CollisionObject collision_object_2;
+    moveit_msgs::msg::CollisionObject collision_object_3;
 
-  collision_object_1.header.frame_id = "panda_link0";
-  collision_object_1.id = "box1";
+    collision_object_1.header.frame_id = "panda_link0";
+    collision_object_1.id = "box1";
 
-  collision_object_2.header.frame_id = "panda_link0";
-  collision_object_2.id = "box2";
+    collision_object_2.header.frame_id = "panda_link0";
+    collision_object_2.id = "box2";
 
-  collision_object_3.header.frame_id = "panda_link0";
-  collision_object_3.id = "box3";
+    collision_object_3.header.frame_id = "panda_link0";
+    collision_object_3.id = "box3";
 
-  shape_msgs::msg::SolidPrimitive box_1;
-  box_1.type = box_1.BOX;
-  box_1.dimensions = { 0.01, 0.12, 1.0 };
+    shape_msgs::msg::SolidPrimitive box_1;
+    box_1.type = box_1.BOX;
+    box_1.dimensions = { 0.01, 0.12, 1.0 };
 
-  shape_msgs::msg::SolidPrimitive box_2;
-  box_2.type = box_2.BOX;
-  box_2.dimensions = { 0.01, 1.0, 0.2 };
+    shape_msgs::msg::SolidPrimitive box_2;
+    box_2.type = box_2.BOX;
+    box_2.dimensions = { 0.01, 1.0, 0.2 };
 
-  // Add new collision objects
-  geometry_msgs::msg::Pose box_pose_1;
-  box_pose_1.position.x = 0.2;
-  box_pose_1.position.y = 0.2;
-  box_pose_1.position.z = 0.7;
+    // Add new collision objects
+    geometry_msgs::msg::Pose box_pose_1;
+    box_pose_1.position.x = 0.2;
+    box_pose_1.position.y = 0.2;
+    box_pose_1.position.z = 0.7;
 
-  collision_object_1.primitives.push_back(box_1);
-  collision_object_1.primitive_poses.push_back(box_pose_1);
-  collision_object_1.operation = collision_object_1.ADD;
+    collision_object_1.primitives.push_back(box_1);
+    collision_object_1.primitive_poses.push_back(box_pose_1);
+    collision_object_1.operation = collision_object_1.ADD;
 
-  geometry_msgs::msg::Pose box_pose_2;
-  box_pose_2.position.x = 0.2;
-  box_pose_2.position.y = -0.2;
-  box_pose_2.position.z = 0.7;
+    geometry_msgs::msg::Pose box_pose_2;
+    box_pose_2.position.x = 0.2;
+    box_pose_2.position.y = -0.2;
+    box_pose_2.position.z = 0.7;
 
-  collision_object_2.primitives.push_back(box_1);
-  collision_object_2.primitive_poses.push_back(box_pose_2);
-  collision_object_2.operation = collision_object_2.ADD;
+    collision_object_2.primitives.push_back(box_1);
+    collision_object_2.primitive_poses.push_back(box_pose_2);
+    collision_object_2.operation = collision_object_2.ADD;
 
-  geometry_msgs::msg::Pose box_pose_3;
-  box_pose_3.position.x = -0.1;
-  box_pose_3.position.y = 0.0;
-  box_pose_3.position.z = 0.85;
+    geometry_msgs::msg::Pose box_pose_3;
+    box_pose_3.position.x = -0.1;
+    box_pose_3.position.y = 0.0;
+    box_pose_3.position.z = 0.85;
 
-  collision_object_3.primitives.push_back(box_2);
-  collision_object_3.primitive_poses.push_back(box_pose_3);
-  collision_object_3.operation = collision_object_3.ADD;
+    collision_object_3.primitives.push_back(box_2);
+    collision_object_3.primitive_poses.push_back(box_pose_3);
+    collision_object_3.operation = collision_object_3.ADD;
 
-  // Add object to planning scene
-  {  // Lock PlanningScene
-    planning_scene_monitor::LockedPlanningSceneRW scene(moveit_cpp_ptr->getPlanningSceneMonitor());
-    scene->processCollisionObjectMsg(collision_object_1);
-    scene->processCollisionObjectMsg(collision_object_2);
-    scene->processCollisionObjectMsg(collision_object_3);
-  }  // Unlock PlanningScene
-};
+    // Add object to planning scene
+    {  // Lock PlanningScene
+      planning_scene_monitor::LockedPlanningSceneRW scene(moveit_cpp_ptr->getPlanningSceneMonitor());
+      scene->processCollisionObjectMsg(collision_object_1);
+      scene->processCollisionObjectMsg(collision_object_2);
+      scene->processCollisionObjectMsg(collision_object_3);
+    }  // Unlock PlanningScene
+  };
 
-// Experiment 1 - Short motion - Cartesian goal
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-RCLCPP_INFO(LOGGER, "################ Short motion - Cartesian goal ##############");
-visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setCartGoal(0.28,-0.2, 0.5, 0.92396, -0.3825, 1.3251e-12, 3.2002e-12);
-printAndPlan(3, false);
+  // Experiment 1 - Short motion - Cartesian goal
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  RCLCPP_INFO(LOGGER, "################ Short motion - Cartesian goal ##############");
+  visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setCartGoal(0.28, -0.2, 0.5, 0.92396, -0.3825, 1.3251e-12, 3.2002e-12);
+  printAndPlan(3, false);
 
-// Experiment 1 - Short motion - Joint goal
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-RCLCPP_INFO(LOGGER, "################ Short motion - Joint goal ##############");
-visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setJointGoal(2.8886513579712823, 0.3875018855212286, 2.720914148047324, -2.876892569361917, 2.886120885454169, 0.6383841437770176, 0.4312752220542212);
-printAndPlan(3, false);
+  // // Experiment 1 - Short motion - Joint goal
+  // // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  RCLCPP_INFO(LOGGER, "################ Short motion - Joint goal ##############");
+  visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setJointGoal(2.8886513579712823, 0.3875018855212286, 2.720914148047324, -2.876892569361917, 2.886120885454169,
+               0.6383841437770176, 0.4312752220542212);
+  printAndPlan(3, false);
 
+  // // Experiment 2 - Same pose - Cartesian goal
+  // // ^^^^^
+  RCLCPP_INFO(LOGGER, "################ Same pose - Cartesian goal ##############");
+  visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setCartGoal(0.30702, -5.2214e-12, 0.59027, 0.92396, -0.3825, 1.3251e-12, 3.2002e-12);
+  printAndPlan(1, true);
+  printAndPlan(2, false);
 
+  //// Experiment 2 - Same pose - Joint goal
+  //// ^^^^^
+  RCLCPP_INFO(LOGGER, "################ Same pose - Joint goal ##############");
+  visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setJointGoal(0.0, -0.785, 0.0, -2.356, 0.0, 1.571, 0.785);
+  printAndPlan(3, false);
 
-// Experiment 2 - Same pose - Cartesian goal
-// ^^^^^
-RCLCPP_INFO(LOGGER, "################ Same pose - Cartesian goal ##############");
-visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setCartGoal(0.30702, -5.2214e-12, 0.59027, 0.92396, -0.3825, 1.3251e-12, 3.2002e-12);
-printAndPlan(1, true);
-printAndPlan(15, false);
+  // // Experiment 3 - Long motion freespace - Joint goal
+  // // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  RCLCPP_INFO(LOGGER, "################ Long motion - Joint goal ##############");
+  visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setJointGoal(-2.583184932292678, -0.290335965663780, -1.030661387231159, -2.171781392507914, 2.897232510573447,
+               1.1244922991023616, 2.708936891424673);
+  printAndPlan(3, false);
 
-// Experiment 2 - Same pose - Joint goal
-// ^^^^^
-RCLCPP_INFO(LOGGER, "################ Same pose - Joint goal ##############");
-visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setJointGoal(0.0, -0.785, 0.0, -2.356, 0.0, 1.571, 0.785);
-printAndPlan(3, false);
+  // // Experiment 3 - Long motion freespace - Cartesian goal
+  // // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  RCLCPP_INFO(LOGGER, "################ Long motion - Cartesian goal ##############");
+  visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setCartGoal(-0.42889, 0.26552, 0.6666, 0.92396, -0.3825, 1.3251e-12, 3.2002e-12);
+  printAndPlan(3, false);
 
-// Experiment 3 - Long motion freespace - Joint goal
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-RCLCPP_INFO(LOGGER, "################ Long motion - Joint goal ##############");
-visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setJointGoal(-2.583184932292678, -0.290335965663780, -1.030661387231159, -2.171781392507914, 2.897232510573447, 1.1244922991023616, 2.708936891424673);
-printAndPlan(3, false);
+  // Experiment 4 - Long motion with collisions - Joint goal
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  RCLCPP_INFO(LOGGER, "################ Long motion - Joint goal ##############");
+  add_collision_objects();
+  visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setJointGoal(-2.583184932292678, -0.290335965663780, -1.030661387231159, -2.171781392507914, 2.897232510573447,
+               1.1244922991023616, 2.708936891424673);
+  printAndPlan(3, false);
 
-// Experiment 3 - Long motion freespace - Cartesian goal
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-RCLCPP_INFO(LOGGER, "################ Long motion - Cartesian goal ##############");
-visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setCartGoal(-0.42889,0.26552,0.6666, 0.92396, -0.3825, 1.3251e-12, 3.2002e-12);
-printAndPlan(3, false);
-
-
-// Experiment 4 - Long motion with collisions - Joint goal
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-RCLCPP_INFO(LOGGER, "################ Long motion - Joint goal ##############");
-add_collision_objects();
-visual_tools.publishText(text_pose, "JointGoal", rvt::WHITE, rvt::XLARGE);
-visual_tools.trigger();
-setJointGoal(-2.583184932292678, -0.290335965663780, -1.030661387231159, -2.171781392507914, 2.897232510573447, 1.1244922991023616, 2.708936891424673);
-printAndPlan(5, false);
-
-  // Experiment 4 - Long motion with collisions - Cartesian goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- RCLCPP_INFO(LOGGER, "################ Long motion - Cartesian goal ##############");
- visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
- visual_tools.trigger();
- setCartGoal(-0.42889,0.26552,0.6666,-0.013696,0.005568,-0.38056,0.92464);
- printAndPlan(5, false);
-
-
-
+  //  // Experiment 4 - Long motion with collisions - Cartesian goal
+  //  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  RCLCPP_INFO(LOGGER, "################ Long motion - Cartesian goal ##############");
+  visual_tools.publishText(text_pose, "CartesianGoal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  setCartGoal(-0.42889, 0.26552, 0.6666, -0.013696, 0.005568, -0.38056, 0.92464);
+  printAndPlan(3, false);
 
   RCLCPP_INFO(LOGGER, "Shutting down.");
   rclcpp::shutdown();
