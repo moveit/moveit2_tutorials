2256f3b2dad295b22f2996aad74127cee17b4e53
Sebastian Jahr <sebastian.jahr@picknik.ai>
2022-12-07 12:14:55 -0700

    Format

--- doc/how_to_guides/parallel_planning/launch/parallel_planning_example.launch.py
+++ doc/how_to_guides/parallel_planning/launch/parallel_planning_example.launch.py
@@ -45,8 +45,11 @@ def launch_setup(context, *args, **kwargs):
         .planning_pipelines("ompl", ["ompl", "chomp", "pilz_industrial_motion_planner"])
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .moveit_cpp(
-            os.path.join(get_package_share_directory("moveit2_tutorials"),
-             "config", "parallel_planning_moveit_cpp.yaml")
+            os.path.join(
+                get_package_share_directory("moveit2_tutorials"),
+                "config",
+                "parallel_planning_moveit_cpp.yaml",
+            )
         )
         .to_moveit_configs()
     )
@@ -69,9 +72,10 @@ def launch_setup(context, *args, **kwargs):
     ompl_planning_pipeline_config["ompl_rrt_star"].update(ompl_planning_yaml)
 
     # Warehouse config
-    sqlite_database = (
-      os.path.join(get_package_share_directory("moveit2_tutorials"),
-             "config", "panda_test_db.sqlite")
+    sqlite_database = os.path.join(
+        get_package_share_directory("moveit2_tutorials"),
+        "config",
+        "panda_test_db.sqlite",
     )
 
     warehouse_ros_config = {
@@ -94,9 +98,10 @@ def launch_setup(context, *args, **kwargs):
     )
 
     # RViz
-    rviz_config_file = (
-            os.path.join(get_package_share_directory("moveit2_tutorials"),
-             "config", "parallel_planning_config.rviz")
+    rviz_config_file = os.path.join(
+        get_package_share_directory("moveit2_tutorials"),
+        "config",
+        "parallel_planning_config.rviz",
     )
 
     rviz_node = Node(
