92df9f90a0d4f14a741e19d096c18813e5ebaa9d
Henning Kayser <henningkayser@picknik.ai>
2023-02-20 12:30:58 +0100

    Fix MotionPlanResponse API (rolling docker) (#611)

--- doc/tutorials/visualizing_in_rviz/hello_moveit.cpp
+++ doc/tutorials/visualizing_in_rviz/hello_moveit.cpp
@@ -70,7 +70,7 @@ int main(int argc, char* argv[])
   // Execute the plan
   if (success)
   {
-    draw_trajectory_tool_path(plan.trajectory_);
+    draw_trajectory_tool_path(plan.trajectory);
     moveit_visual_tools.trigger();
     prompt("Press 'next' in the RvizVisualToolsGui window to execute");
     draw_title("Executing");
