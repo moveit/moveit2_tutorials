d48f3f4079eadab40bb7be977a405fab547ab149
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-03-31 14:43:37 +0200

    Use interpolation planner instead of sampling planner for hand

--- doc/tutorials/pick_and_place_with_moveit_task_constructor/src/mtc_node.cpp
+++ doc/tutorials/pick_and_place_with_moveit_task_constructor/src/mtc_node.cpp
@@ -116,6 +116,7 @@ mtc::Task MTCTaskNode::createTask()
   task.add(std::move(stage_state_current));
 
   auto sampling_planner = std::make_shared<mtc::solvers::PipelinePlanner>(node_);
+
   auto interpolation_planner = std::make_shared<mtc::solvers::JointInterpolationPlanner>();
 
   auto cartesian_planner = std::make_shared<mtc::solvers::CartesianPath>();
@@ -259,7 +260,7 @@ mtc::Task MTCTaskNode::createTask()
     auto stage_move_to_place = std::make_unique<mtc::stages::Connect>(
         "move to place",
         mtc::stages::Connect::GroupPlannerVector{ { arm_group_name, sampling_planner },
-                                                  { hand_group_name, sampling_planner } });
+                                                  { hand_group_name, interpolation_planner } });
     // clang-format on
     stage_move_to_place->setTimeout(5.0);
     stage_move_to_place->properties().configureInitFrom(mtc::Stage::PARENT);
