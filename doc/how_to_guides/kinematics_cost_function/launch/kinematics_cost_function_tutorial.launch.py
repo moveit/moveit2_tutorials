95572903c78a120898365f10760b5abcd5d4b6fa
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-04-27 16:28:30 +0200

    Continuation of "Adding kinematics cost function tutorial #462" (#669)
    Co-authored-by: Wyatt Rees <wyatt.rees@gmail.com>
    Co-authored-by: Sebastian Castro <4603398+sea-bass@users.noreply.github.com>

---
+++ doc/how_to_guides/kinematics_cost_function/launch/kinematics_cost_function_tutorial.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_moveit_configs()
+
+    move_group_demo = Node(
+        name="kinematics_cost_fn_tutorial",
+        package="moveit2_tutorials",
+        executable="kinematics_cost_function_tutorial",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
+        ],
+    )
+
+    return LaunchDescription([move_group_demo])
