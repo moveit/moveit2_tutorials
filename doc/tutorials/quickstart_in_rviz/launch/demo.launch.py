a7a447cc087a0a176f7da0388936f08ac1080694
Anthony Baker <abake48@gmail.com>
2023-07-27 11:21:44 -0600

    update trajectory_execution param

--- doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
+++ doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
@@ -40,6 +40,7 @@ def launch_setup(context, *args, **kwargs):
             "gen3", package_name="kinova_gen3_7dof_robotiq_2f_85_moveit_config"
         )
         .robot_description(mappings=launch_arguments)
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .planning_scene_monitor(
             publish_robot_description=True, publish_robot_description_semantic=True
         )
