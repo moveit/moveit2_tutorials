c2b673e228b104b855b31cd79fc93f20af02c307
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-10-09 18:22:03 +0200

    Format

--- doc/examples/motion_planning_pipeline/launch/motion_planning_pipeline_tutorial.launch.py
+++ doc/examples/motion_planning_pipeline/launch/motion_planning_pipeline_tutorial.launch.py
@@ -5,6 +5,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
 
+
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
