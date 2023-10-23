ae2996292ebaca4e5546583232a5f2e1ec6756f2
Sebastian Castro <sebas.a.castro@gmail.com>
2023-10-23 13:52:10 -0400

    Update instructions for Pilz tutorial

--- doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
+++ doc/tutorials/quickstart_in_rviz/launch/demo.launch.py
@@ -2,10 +2,8 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
 
