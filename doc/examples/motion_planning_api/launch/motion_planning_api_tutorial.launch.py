3033253006a635b48d06a3ce71dccbcc92461a17
Paul Gesel <paul.gesel@picknik.ai>
2023-12-05 15:59:17 -0700

    run pre-commit
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>

--- doc/examples/motion_planning_api/launch/motion_planning_api_tutorial.launch.py
+++ doc/examples/motion_planning_api/launch/motion_planning_api_tutorial.launch.py
@@ -6,11 +6,11 @@ from moveit_configs_utils import MoveItConfigsBuilder
 def generate_launch_description():
     moveit_config = (
         MoveItConfigsBuilder("moveit_resources_panda")
-            .robot_description(file_path="config/panda.urdf.xacro")
-            .robot_description_semantic(file_path="config/panda.srdf")
-            .trajectory_execution(file_path="config/moveit_controllers.yaml")
-            .planning_pipelines(pipelines=["ompl"])
-            .to_moveit_configs()
+        .robot_description(file_path="config/panda.urdf.xacro")
+        .robot_description_semantic(file_path="config/panda.srdf")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
     )
 
     return LaunchDescription(
