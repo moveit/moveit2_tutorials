11710ca28f99c4a38ab18313464f45e93f3c4ed0
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-12-07 13:12:02 -0700

    Update tutorials to pipeline refactoring changes (#834)
    * Update tutorials

--- doc/how_to_guides/parallel_planning/launch/parallel_planning_example.launch.py
+++ doc/how_to_guides/parallel_planning/launch/parallel_planning_example.launch.py
@@ -57,15 +57,20 @@ def launch_setup(context, *args, **kwargs):
     # Load additional OMPL pipeline
     ompl_planning_pipeline_config = {
         "ompl_2": {
-            "planning_plugin": "ompl_interface/OMPLPlanner",
-            "request_adapters": """\
-                default_planner_request_adapters/AddTimeOptimalParameterization \
-                default_planner_request_adapters/FixWorkspaceBounds \
-                default_planner_request_adapters/FixStartStateBounds \
-                default_planner_request_adapters/FixStartStateCollision \
-                default_planner_request_adapters/FixStartStatePathConstraints \
-              """,
-            "start_state_max_bounds_error": 0.1,
+            "planning_plugins": [
+                "ompl_interface/OMPLPlanner",
+            ],
+            "request_adapters": [
+                "default_planning_request_adapters/ResolveConstraintFrames",
+                "default_planning_request_adapters/ValidateWorkspaceBounds",
+                "default_planning_request_adapters/CheckStartStateBounds",
+                "default_planning_request_adapters/CheckStartStateCollision",
+            ],
+            "response_adapters": [
+                "default_planning_response_adapters/AddTimeOptimalParameterization",
+                "default_planning_response_adapters/ValidateSolution",
+                "default_planning_response_adapters/DisplayMotionPath",
+            ],
         }
     }
     ompl_planning_yaml = load_yaml(
